e7199eabff95b6c08ef709025332b2c848d79355
Anna Hwang <anna.s.hwang@vanderbilt.edu>
2015-05-06 00:06:28 -0500

    finished for now i suppose

--- lazy.js
+++ lazy.js
@@ -5,23 +5,28 @@
  *          Makes bootup a bit faster and Chrome take up less memory.
  */
 
+//console.log("LAZYTABS");
+
 // the object of active tabs
-console.log("LAZYTABS");
 var activeTabs = {};
 
 // add the current selected tab
 chrome.tabs.query( {active: true, currentWindow: true}, function(tab) {
+    /*
     console.log("query adding tab: " + tab.id);
     console.log("tab url: " + tab.url);
+    */
     activeTabs[tab.id] = 1;
 });
 
 // body check every webrequest make sure they're authorized
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
+        /*
         console.log("request from tab: " + details.tabId);
         console.log("tab url: " + details.url);
         console.log(details.tabId != -1 && activeTabs[details.tabId] == null);
+        */
         return {cancel: details.tabId != -1 && activeTabs[details.tabId] == null};
     },
     {urls: ["<all_urls>"]},
@@ -30,9 +35,13 @@ chrome.webRequest.onBeforeRequest.addListener(
 
 // when a tab is selected, add it to activeTabs if it is not already there
 chrome.tabs.onActivated.addListener(function(activeInfo) {
+    /*
     console.log("onActivated from tab: " + activeInfo.tabId);
+    */
     if(activeTabs[activeInfo.tabId] == null) {
+        /*
         console.log("(ADDED) tab url: " + activeInfo.url);
+        */
         activeTabs[activeInfo.tabId] = 1;
         chrome.tabs.reload(activeInfo.tabId);
     }
@@ -40,6 +49,8 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
 
 // remove any tab from activeTabs that is closed cuz they are not relevant anymore
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    /*
     console.log("removed tab: " + tabId);
+    */
     delete activeTabs[tabId];
 });
