39f600b806730f15a6a2edac2c18365d8c1d22b2
Anna Hwang <anna.s.hwang@vanderbilt.edu>
2015-05-05 23:44:47 -0500

    ???

--- lazy.js
+++ lazy.js
@@ -6,23 +6,23 @@
  */
 
 // the object of active tabs
+console.log("LAZYTABS");
 var activeTabs = {};
-var empty = true;
 
 // add the current selected tab
 chrome.tabs.query( {active: true, currentWindow: true}, function(tab) {
-    if(empty == true) {
-        activeTabs[tab.id] = 1;
-        chrome.tabs.reload(activeInfo.tabId);
-        empty = false;
-    }
+    console.log("query adding tab: " + tab.id);
+    console.log("tab url: " + tab.url);
+    activeTabs[tab.id] = 1;
 });
 
 // body check every webrequest make sure they're authorized
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        var value = activeTabs[details.tabId] == null;
-        return {cancel: value};
+        console.log("request from tab: " + details.tabId);
+        console.log("tab url: " + details.url);
+        console.log(details.tabId != -1 && activeTabs[details.tabId] == null);
+        return {cancel: details.tabId != -1 && activeTabs[details.tabId] == null};
     },
     {urls: ["<all_urls>"]},
     ["blocking"]
@@ -30,19 +30,16 @@ chrome.webRequest.onBeforeRequest.addListener(
 
 // when a tab is selected, add it to activeTabs if it is not already there
 chrome.tabs.onActivated.addListener(function(activeInfo) {
+    console.log("onActivated from tab: " + activeInfo.tabId);
     if(activeTabs[activeInfo.tabId] == null) {
+        console.log("(ADDED) tab url: " + activeInfo.url);
         activeTabs[activeInfo.tabId] = 1;
-        //don't refresh here it breaks things for some fucking reason
-        //unfortunately if you don't refresh then the user has to wait a bit for
-        //the page to naturally refresh itself due to incoming requests
-        //(length of wait depends on when the next request naturally comes in)
-        //OR MAYBE ITS NOT AN ISSUE
-        //???
         chrome.tabs.reload(activeInfo.tabId);
     }
 });
 
 // remove any tab from activeTabs that is closed cuz they are not relevant anymore
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    console.log("removed tab: " + tabId);
     delete activeTabs[tabId];
 });
